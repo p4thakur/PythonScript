646e27e753e9b1149658431e0b566f7a589f5e34
p4thakur <36537509+p4thakur@users.noreply.github.com>
2021-08-19 20:26:07 +0530

    Adding update statement with multiple lines

--- Excel_to_SQLUpdate.py
+++ Excel_to_SQLUpdate.py
@@ -15,3 +15,40 @@ for i in range(len(accountnumber)):
   f.write(a)
   f.write("\n")
   
+  
+  /******
+  # #Script to make sql update statement from csv
+# import pandas
+
+# df = pandas.read_excel('dueAmount.xls')
+
+# list1=df.columns.ravel();
+# #print(df['account_number'].tolist())
+# due_date= df['due_date'];
+# accountnumber=df['account_number'];
+# print(len(accountnumber))
+# for i in range(len(accountnumber)):
+#   a='update accounts set due_date=\''+str(due_date[i])+ '\' where account_number=\''+str(accountnumber[i])+'\';'
+#   f = open("demo.txt", "a")
+#   f.write(a)
+#   f.write("\n")
+#Script to make sql update statement from csv
+import pandas
+
+df = pandas.read_excel('dueAmount.xls')
+
+list1=df.columns.ravel();
+#print(df['account_number'].tolist())
+due_date1= df['due_date'];
+accountnumber=df['account_number'];
+print(len(accountnumber))
+for i in range(len(accountnumber)):
+  # a='update accounts set due_date=\''+str(due_date[i])+ '\' where account_number=\''+str(accountnumber[i])+'\';'
+  a='INSERT INTO manual_update_audit (loan_account_number,`column`, `table`, update_type, old_value, new_value) '\
+     'SELECT account_number, "due_date","accounts", "UPDATE", '\
+     'JSON_OBJECT( "due_date",due_date ),JSON_OBJECT( "due_date",\''+str(due_date1[i])+'\') FROM accounts '\
+      'where account_number=\''+str(accountnumber[i])+'\';'
+  f = open("manualAudit.txt", "a")
+  f.write(a)
+  f.write("\n")
+  ********/
